f0ef315df7662a7c6f578ebd39bf8de6d029d42d
Blaise Swanwick <blaise@advancedkiosks.com>
2025-01-21 09:34:54 -0500

    Incremental update with tile.png and #home-content

--- scripts.js
+++ scripts.js
@@ -4,12 +4,16 @@ console.log(
 ██╔══██╗██║     ██╔══██╗╚══███╔╝██╔════╝
 ██████╔╝██║     ███████║  ███╔╝ █████╗  
 ██╔══██╗██║     ██╔══██║ ███╔╝  ██╔══╝  
-██████║ ███████╗██║  ██║███████╗███████╗
+██████╔╝███████╗██║  ██║███████╗███████╗
 ╚═════╝ ╚══════╝╚═╝  ╚═╝═══════╝╚══════╝
 `,
-  "color: cyan; font-weight: bold;"
+  "color: #2978a0; font-weight: bold;"
 );
-console.log("...was here! 👩🏻‍💻");
+console.log("...was here!");
+console.warn("This is a hobby site. Please don't take it too seriously! 😅");
+console.log(
+  "My aim was to recreate a sense of nostalgia from the 90s. In the early days of the internet, websites were a lot more fun to explore.  Platforms like Angelfire and GeoCities allowed anyone to publish their own website.  And in those early days, the creativity and personal pride put into every site was just so inspiring!  Now, I'm the webmaster. This site is a faithful ode to those early pioneers. Happy gunting! 🌈"
+);
 
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(SlowMo);
