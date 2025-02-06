a5f0d208fd15c2f3f61623e3582eb33d7c006cb6
Blaise Swanwick <blaise@advancedkiosks.com>
2025-02-06 16:07:45 -0500

    hidden tile + css + fonts

--- scripts.js
+++ scripts.js
@@ -15,6 +15,17 @@ console.log(
   "My aim was to recreate a sense of nostalgia from the 90s. In the early days of the internet, websites were a lot more fun to explore.  Platforms like Angelfire and GeoCities allowed anyone to publish their own website.  And in those early days, the creativity and personal pride put into every site was just so inspiring!  Now, I'm the webmaster. This site is a faithful ode to those early pioneers. Happy gunting! 🌈"
 );
 
+function say(message) {
+  const utterance = new SpeechSynthesisUtterance(message);
+  const voices = speechSynthesis.getVoices(),
+    l = voices.length;
+  if (l > 1) {
+    utterance.voice = voices[(Math.random() * l) | 0];
+  }
+  utterance.rate = 1.2;
+  speechSynthesis.speak(utterance);
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(SlowMo);
 
@@ -28,4 +39,18 @@ document.addEventListener("DOMContentLoaded", (event) => {
     ease: Linear.easeNone,
     duration: 3,
   });
+});
+
+document.addEventListener("click", (ev) => {
+  if (ev.target.id === "tile") {
+    const text =
+      "Congratulations to you! You have found a hidden tile. I hope you keep looking for more. I have hidden a few more around the site. Good luck finding them all! If you find all of them, you will be rewarded. Happy gunting!";
+    // alert(`${text} 🎉`);
+    say(text);
+  } else {
+    var tl = new TimelineMax();
+    tl.to("#tile", {
+      rotation: "+=360",
+    });
+  }
 });
